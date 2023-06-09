5c3d511a21c9f7a8fc769ddb95ff89e9149df95f
Marwan1011994 <112905329+Marwan1011994@users.noreply.github.com>
2023-06-09 17:07:18 +0300

    Add files via upload

--- js/chartScript.js
+++ js/chartScript.js
@@ -2,10 +2,108 @@
 // const ctx = document.getElementById('myChart');
 // const ctn = document.getElementById('myChart2');
 
+// كميات المياه المنتجه من جميع المحطات
+const allStations = document.getElementById('myChart1');
+new Chart(allStations, {
+    type: 'bar',
+    data: {
+      labels: ['متوسط المحطات المرشحة الكبري','متوسط المحطات المرشحة الصغري والنقالى','متوسط المحطات الارتوازى'],
+      datasets: [{
+        label: 'متوسط كمية المياه المنتجه م3/يوم',
+        data: [442403,64122,148068],
+        backgroundColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 205, 86, 1)',
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'متوسط انتاج المحطات',
+          }
+        }
+      },
+  });
+
+
+// نسبة المياه المنتجه من جميع المحطات
+const allStationsprcent = document.getElementById('myChart2');
+new Chart(allStationsprcent, {
+    type: 'pie',
+    data: {
+      labels: ['متوسط المحطات المرشحة الكبري','متوسط المحطات المرشحة الصغري والنقالى','متوسط المحطات الارتوازى'],
+      datasets: [{
+        label: "% Prcent",
+        data: [67.58,9.80,22.62],
+        borderWidth: 1
+      }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'bottom',
+          },
+          title: {
+            display: true,
+            text: 'النسبة المئوية لانتاج المحطات'
+          }
+        }
+      },
+  });
+
+
+//كمية المياه المنتجه من الافرع والمباعه
+// const branches = document.getElementById('myChart3');
+// new Chart(branches, {
+//     type: 'bar',
+//     data: {
+//       labels: ['مدينة بنها','مركز بنها'],
+//       datasets: [{
+//         label: 'كمية المياه المنتجه م3/يوم',
+//         data: [{
+//           data:[30,40],
+//           borderColor: Utils.CHART_COLORS.red,
+//           backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+//           borderWidth: 2,
+//         },{
+//           data:[50,60],
+//           borderColor: Utils.CHART_COLORS.red,
+//           backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+//           borderWidth: 2,
+//         }],
+//         backgroundColor: [
+//           'rgba(255, 99, 132, 1)',
+//           'rgba(255, 159, 64, 1)',
+//           'rgba(255, 205, 86, 1)',
+//           'rgba(75, 192, 192, 1)',
+//           'rgba(54, 162, 235, 1)',
+//           'rgba(153, 102, 255, 1)',
+//           'rgba(100, 102, 255, 1)',
+//         ],
+//     options: {
+//         responsive: true,
+//         plugins: {
+//           legend: {
+//             position: 'top',
+//           },
+//           title: {
+//             display: true,
+//             text: 'المحطات المرشحة الكبري',
+//           }
+//         }
+//       },
+//   });
 // المحطات المرشحة الكبري
-
-const bigStations = document.getElementById('myChart1');
-
+const bigStations = document.getElementById('myChart3');
 new Chart(bigStations, {
     type: 'bar',
     data: {
