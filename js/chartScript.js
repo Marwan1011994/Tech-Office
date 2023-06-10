fecbb56d83bed57388c69e6d159ff8159aa48205
Marwan1011994 <112905329+Marwan1011994@users.noreply.github.com>
2023-06-10 20:37:33 +0300

    Add files via upload

--- js/chartScript.js
+++ js/chartScript.js
@@ -1,6 +1,4 @@
 
-// const ctx = document.getElementById('myChart');
-// const ctn = document.getElementById('myChart2');
 
 // كميات المياه المنتجه من جميع المحطات
 const allStations = document.getElementById('myChart1');
@@ -12,10 +10,7 @@ new Chart(allStations, {
         label: 'متوسط كمية المياه المنتجه م3/يوم',
         data: [442403,64122,148068],
         backgroundColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(255, 205, 86, 1)',
-        ],
+          'blue'],
         borderWidth: 1
       }]
     },
@@ -27,9 +22,23 @@ new Chart(allStations, {
           },
           title: {
             display: true,
-            text: 'متوسط انتاج المحطات',
+            text: 'متوسط انتاج المحطات' +" "+"الربع الثالث",
           }
-        }
+        },
+        animations: {
+          radius: {
+            duration: 400,
+            easing: 'linear',
+            loop: (context) => context.active
+          }
+        },
+        hoverRadius: 12,
+        hoverBackgroundColor: '#ff6000',
+        interaction: {
+          mode: 'nearest',
+          intersect: false,
+          axis: 'x'
+        },
       },
   });
 
@@ -41,9 +50,9 @@ new Chart(allStationsprcent, {
     data: {
       labels: ['متوسط المحطات المرشحة الكبري','متوسط المحطات المرشحة الصغري والنقالى','متوسط المحطات الارتوازى'],
       datasets: [{
-        label: "% Prcent",
+        label: "المياه المنتجه %",
         data: [67.58,9.80,22.62],
-        borderWidth: 1
+        borderWidth: 3
       }]
     },
     options: {
@@ -54,72 +63,84 @@ new Chart(allStationsprcent, {
           },
           title: {
             display: true,
-            text: 'النسبة المئوية لانتاج المحطات'
+            text: 'النسبة المئوية لانتاج المحطات'+" "+"الربع الثالث",
           }
-        }
+        },
+        animations: {
+          radius: {
+            duration: 400,
+            easing: 'linear',
+            loop: (context) => context.active
+          }
+        },
+        hoverRadius: 12,
+        hoverBackgroundColor: '#fff0f9',
+        interaction: {
+          mode: 'nearest',
+          intersect: false,
+          axis: 'x'
+        },
       },
   });
 
 
 //كمية المياه المنتجه من الافرع والمباعه
-// const branches = document.getElementById('myChart3');
-// new Chart(branches, {
-//     type: 'bar',
-//     data: {
-//       labels: ['مدينة بنها','مركز بنها'],
-//       datasets: [{
-//         label: 'كمية المياه المنتجه م3/يوم',
-//         data: [{
-//           data:[30,40],
-//           borderColor: Utils.CHART_COLORS.red,
-//           backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-//           borderWidth: 2,
-//         },{
-//           data:[50,60],
-//           borderColor: Utils.CHART_COLORS.red,
-//           backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-//           borderWidth: 2,
-//         }],
-//         backgroundColor: [
-//           'rgba(255, 99, 132, 1)',
-//           'rgba(255, 159, 64, 1)',
-//           'rgba(255, 205, 86, 1)',
-//           'rgba(75, 192, 192, 1)',
-//           'rgba(54, 162, 235, 1)',
-//           'rgba(153, 102, 255, 1)',
-//           'rgba(100, 102, 255, 1)',
-//         ],
-//     options: {
-//         responsive: true,
-//         plugins: {
-//           legend: {
-//             position: 'top',
-//           },
-//           title: {
-//             display: true,
-//             text: 'المحطات المرشحة الكبري',
-//           }
-//         }
-//       },
-//   });
-// المحطات المرشحة الكبري
-const bigStations = document.getElementById('myChart3');
-new Chart(bigStations, {
+const branches = document.getElementById('myChart3');
+new Chart(branches, {
     type: 'bar',
     data: {
-      labels: ['اتريب المرشحه', 'العبادله المرشحه', 'كوم اشفين المرشحه', 'شبين القناطر المرشحه', 'القناطر الخيرية المرشحه', 'ابو زعبل المرشحه', 'سرياقوس المرشحه'],
+      labels: ['بنها','كفر شكر','طوخ','قها','قليوب','شبين القناطر','القناطر الخيرية','الخانكة'],
       datasets: [{
-        label: 'كمية المياه المنتجه م3/يوم',
-        data: [55213, 93110, 79096, 79469, 54199, 42011,48305],
-        backgroundColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(255, 205, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(100, 102, 255, 1)',
-        ],
+        label: 'كمية المياه المنتجه',
+        data: [12020491,3220825,9675103,527541,6608224,8608224,8781248,11113776],
+        backgroundColor: ["blue"],
+        borderWidth: 1
+      },{
+        label: 'كمية المياه المباعة',
+        data: [8847946,2499940,7350848,427134,5888213,6135985,4057176,3977151],
+        backgroundColor: ["green"],
+        borderWidth: 1,
+      }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'متوسط كمية المياه المنتجه والمباعة' +" "+"الربع الثالث",
+          }
+        },
+        animations: {
+          radius: {
+            duration: 400,
+            easing: 'linear',
+            loop: (context) => context.active
+          }
+        },
+        hoverRadius: 12,
+        hoverBackgroundColor: '#fff',
+        interaction: {
+          mode: 'nearest',
+          intersect: false,
+          axis: 'x'
+        },
+      },
+  });
+
+
+//الفاقد 
+const waterLoss = document.getElementById('myChart4');
+new Chart(waterLoss, {
+    type: 'bar',
+    data: {
+      labels: ['بنها','كفر شكر','طوخ','قها','قليوب','شبين القناطر','القناطر الخيرية','الخانكة'],
+      datasets: [{
+        label: ["الفاقد %"],
+        data: [26,22,24,19,12,29,54,64],
+        backgroundColor: ["#068DA9"],
         borderWidth: 1
       }]
     },
@@ -131,58 +152,117 @@ new Chart(bigStations, {
           },
           title: {
             display: true,
-            text: 'المحطات المرشحة الكبري',
+            text: 'متوسط فاقد الفروع' +" "+"الربع الثالث",
           }
-        }
+        },
+        animations: {
+          radius: {
+            duration: 400,
+            easing: 'linear',
+            loop: (context) => context.active
+          }
+        },
+        hoverRadius: 12,
+        hoverBackgroundColor: '#0000ff',
+        interaction: {
+          mode: 'nearest',
+          intersect: false,
+          axis: 'x'
+        },
       },
   });
 
-// new Chart(ctn, {
-//     type: 'line',
-//     data: {
-//       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//       datasets: [{
-//         label: '# of Votes',
-//         data: [12, 19, 3, 5, 2, 3],
-//         borderWidth: 1
-//       }]
-//     },
-//     options: {
-//         responsive: true,
-//         plugins: {
-//           legend: {
-//             position: 'top',
-//           },
-//           title: {
-//             display: true,
-//             text: 'Chart.js Line Chart'
-//           }
-//         }
-//       },
-//   });
-// new Chart(ctx, {
-//     type: 'pie',
-//     data: {
-//       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//       datasets: [{
-//         label: '# of Votes',
-//         data: [12, 19, 3, 5, 2, 3],
-//         borderWidth: 1
-//       }]
-//     },
-//     options: {
-//         responsive: true,
-//         plugins: {
-//           legend: {
-//             position: 'bottom',
-//           },
-//           title: {
-//             display: true,
-//             text: 'Chart.js Pie Chart'
-//           }
-//         }
-//       },
-//   });
+// المحطات المرشحة الكبري
+const bigStations = document.getElementById('myChart5');
+new Chart(bigStations, {
+    type: 'bar',
+    data: {
+      labels: ['اتريب المرشحه', 'العبادله المرشحه', 'كوم اشفين المرشحه', 'شبين القناطر المرشحه', 'القناطر الخيرية المرشحه', 'ابو زعبل المرشحه', 'سرياقوس المرشحه'],
+      datasets: [{
+        label: 'متوسط كمية المياه المنتجه م3/يوم',
+        data: [55213, 93110, 79096, 79469, 54199, 42011,48305],
+        backgroundColor: [
+          'blue'],
+        borderWidth: 1
+      }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'المحطات المرشحة الكبري'+" "+"الربع الثالث",
+          }
+        },
+        animations: {
+          radius: {
+            duration: 400,
+            easing: 'linear',
+            loop: (context) => context.active
+          }
+        },
+        hoverRadius: 12,
+        hoverBackgroundColor: '#9E4784',
+        interaction: {
+          mode: 'nearest',
+          intersect: false,
+          axis: 'x'
+        },
+      },
+  });
 
+//كمية المياه المنتجه من الافرع والمباعه
+const clPower = document.getElementById('myChart6');
+new Chart(clPower, {
+    type: 'bar',
+    data: {
+      labels: ['اتريب المرشحه', 'العبادله المرشحه', 'كوم اشفين المرشحه', 'شبين القناطر المرشحه', 'القناطر الخيرية المرشحه', 'ابو زعبل المرشحه', 'سرياقوس المرشحه'],
+      datasets: [{
+        label: 'متوسط استهلاك الكلور جم/م3/يوم',
+        data: [122491,32025,97513,52441,668224,86224,87148,111776],
+        backgroundColor: ["#E14D2A"],
+        borderWidth: 1
+      },{
+        label: 'متوسط استهلاك الشبة جم/م3/يوم',
+        data: [8846,2440,7348,4534,5813,6185,4076,3951],
+        backgroundColor: ["#CD104D"],
+        borderWidth: 1,
+      },{
+        label: 'متوسط استهلاك الكهرباء ك.و/م3/يوم',
+        data: [884946,249940,75048,427134,588823,613585,405776,377151],
+        backgroundColor: ["#9C2C77"],
+        borderWidth: 1,
+      }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'متوسط اسنهلاك الشبة والكلور والكهرباء للمحطات المرشحة الكبرى' +" "+"الربع الثالث",
+          }
+        },
+        animations: {
+          radius: {
+            duration: 400,
+            easing: 'linear',
+            loop: (context) => context.active
+          }
+        },
+        hoverRadius: 12,
+        hoverBackgroundColor: '#fff',
+        interaction: {
+          mode: 'nearest',
+          intersect: false,
+          axis: 'x'
+        },
+      },
+  });
 
 
